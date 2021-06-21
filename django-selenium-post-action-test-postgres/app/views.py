8858a19ea8bec883729a03bba38a0014fec68332
Penguin135 <rudwns273@naver.com>
2021-06-21 14:15:22 +0900

    add project including postgres

---
+++ django-selenium-post-action-test-postgres/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import ProfileForm
+from .models import Profile
+# Create your views here.
+def home(request):
+    # return render(request, 'home.html')
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    form = ProfileForm
+    profiles = Profile.objects.all()
+    return render(request, 'home.html', {'form': form, 'profiles': profiles})
