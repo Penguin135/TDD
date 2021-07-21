17dababca37ffe4a9a612bc1d782d612c9504533
Penguin135 <rudwns273@naver.com>
2021-07-21 11:57:53 +0900

    EDIT app/tests.py - reduce same testcase

--- django-selenium-post-action-test-postgres/app/tests.py
+++ django-selenium-post-action-test-postgres/app/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
-# Create your tests here.
 
 class BaseTestCase(TestCase):
     def setUp(self):
